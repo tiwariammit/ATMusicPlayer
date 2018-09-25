4c5325f7365869df863ddf662b98714d983df5b7
Amrit Tiwari <amrit.tiwari@newitventure.com>
2018-09-25 09:16:23 +0545

    Initial Commit

---
+++ ATMusicPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ATMusicPlayer
+//
+//  Created by Amrit Tiwari on 9/25/18.
+//  Copyright © 2018 Amrit Tiwari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
